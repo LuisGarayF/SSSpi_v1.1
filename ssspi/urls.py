e249b5f10a0fc3bc174506b7bd91e64f690a0381
LuisGarayF <luis.alexander.garay@gmail.com>
2023-04-05 21:49:02 -0400

    [ADD] Templates,  views and controllers

--- ssspi/urls.py
+++ ssspi/urls.py
@@ -20,4 +20,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('formsim/', include('formapp.urls')),
+    path('',views.index,name='index'),
 ]
